2c781d964257ffde90c191082ea2c64940d8bc56
QuelGnaro <quelgnaro@gmail.com>
2025-01-22 17:23:05 +0100

    feat: added copy mail btn and tooltip, removed junk

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { PreferencesService } from '../../services/preferences.service';
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  @ViewChild(NgbTooltip) tooltip: NgbTooltip | undefined;
 
   contacts = [
-    { name: 'barartti.lorenzo3@gmail.com', icon: 'envelope', pre: 'fas' },
-    // { name: 'Seveso, MB', icon: 'map-pin', pre: 'fas' },
-    // { name: 'Fulltime / Freelancer', icon: 'suitcase', pre: 'fas' },
+    { name: 'info@lorenzobaratti.it', icon: 'envelope', pre: 'fas' },
     { name: 'LinkedIn', icon: 'linkedin', pre: 'fab', url: 'https://www.linkedin.com/in/baratti-lorenzo/' },
     { name: 'TikTok', icon: 'tiktok', pre: 'fab', url: 'https://www.linkedin.com/in/baratti-lorenzo/' },
     { name: 'Instagram', icon: 'instagram', pre: 'fab', url: 'https://www.linkedin.com/in/baratti-lorenzo/' },
@@ -19,15 +19,72 @@ export class FooterComponent {
   ];
 
   year = new Date().getFullYear();
+  copyTitle: string = '';
+  language: string = '';
+  constructor(private preferencesService: PreferencesService) { }
 
-  constructor() { }
+  ngOnInit() {
+    // Iscriviti al flusso della lingua e aggiorna copyTitle quando cambia
+    this.preferencesService.language$.subscribe(language => {
+      this.updateTooltipTextLanguage(language);
+      this.language = language;
+    });
+  }
 
-  downloadCV() {
-    if (localStorage.getItem('language') === 'en') {
-      window.open('assets/cv/cv_en.pdf', '_blank');
-    }
-    if (localStorage.getItem('language') === 'it') {
-      window.open('assets/cv/CV ITA.pdf', '_blank');
+
+
+  updateTooltipTextLanguage(language: string) {
+    if (language === 'en') {
+      this.copyTitle = 'Copy e-mail!';
+    } else {
+      this.copyTitle = 'Copia e-mail!';
     }
   }
+
+  // Funzione che aggiorna il testo del tooltip in base alla lingua
+  updateTooltipText(language: string) {
+    if (language === 'en') {
+      this.copyTitle = 'Copied!';
+      setTimeout(() => {
+        this.copyTitle = 'Copy e-mail!';
+        if (this.tooltip) {
+          this.tooltip.close();
+        }
+      }, 2000);
+    } else {
+      this.copyTitle = 'Copiata!';
+      setTimeout(() => {
+        this.copyTitle = 'Copia e-mail!';
+        if (this.tooltip) {
+          this.tooltip.close();
+        }
+      }, 2000);
+    }
+  }
+
+  downloadCV() {
+    if (this.language === 'en') {
+      window.open('assets/cv/cv_en.pdf', '_blank');
+    } else
+      window.open('assets/cv/CV ITA.pdf', '_blank');
+  }
+
+  email: string = 'info@lorenzobaratti.it';
+
+  copyText() {
+    const textToCopy = this.email;
+    // Usa la Clipboard API per copiare il testo
+    navigator.clipboard.writeText(textToCopy).then(
+      () => {
+        this.updateTooltipText(this.language);
+      },
+      (err) => {
+        console.error('Errore durante la copia del testo:', err);
+      }
+    );
+  }
+
+
+
+
 }
